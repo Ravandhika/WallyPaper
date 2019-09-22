514937fca4a8981bae7de5f04aa40be0ba612eda
Ravandhika <ravandhika123@gmail.com>
2019-09-22 21:12:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wallypaper/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.wallypaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import andr
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
